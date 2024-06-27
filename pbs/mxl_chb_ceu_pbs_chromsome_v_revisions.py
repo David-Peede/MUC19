0ac8c1733f4485823e0a8ed54981151d84fe1028
David-Peede <david_peede@brown.edu>
2024-06-27 14:12:43 -0400

    Updated PBS code 🎋

---
+++ pbs/mxl_chb_ceu_pbs_chromsome_v_revisions.py
@@ -0,0 +1,101 @@
+# Import packages.
+import allel
+import numpy as np
+import numcodecs
+import pandas as pd
+import sys
+import zarr
+
+### sys.argv[1] = chromosome ###
+
+
+# Define a function to load genotyope and positions arrays.
+def load_gt_pos(prefix, chrom):
+    # Intialize the file path.
+    path = f'../zarr_data/{prefix}_chr{chrom}.zarr'
+    # Load the zarr array.
+    zarr_array = zarr.open_group(path, mode='r')
+    # Extract the genotype callset.
+    callset = zarr_array[f'{chrom}/calldata/GT']
+    # Load the positions.
+    pos = allel.SortedIndex(zarr_array[f'{chrom}/variants/POS'])
+    # Convert the genotype callset to an array.
+    gt = allel.GenotypeArray(callset)
+    # Load the positions.
+    pos = allel.SortedIndex(zarr_array['{0}/variants/POS'.format(chrom)])
+    return gt, pos
+
+# Define a function to compute Hudson's estimator of Fst per site
+def calc_fst_per_site(ac1, ac2):
+    # Compute.
+    num, den = allel.hudson_fst(ac1, ac2)
+    raw_fst = num / den
+    # Correct for when the denominator is zero.
+    raw_fst = np.where(den == 0, 0, raw_fst)
+    # Correct for negative Fst values.
+    raw_fst = np.where(raw_fst < 0, 0, raw_fst)
+    # Correct for Fst values of 1 that will lead to inf.
+    raw_fst = np.where(raw_fst == 1, 0.99999, raw_fst)
+    return raw_fst
+
+# Define a function to calculate PBS.
+def calc_pbs_per_site(gt, pop_a, pop_b, pop_c):
+    # Determine the sample list.
+    samp_list = np.concatenate([pop_a, pop_b, pop_c])
+    # Determine if any of the sites are segregating.
+    seg_mask = gt.take(samp_list, axis=1).count_alleles().is_segregating()
+    # Determine allele counts.
+    a_ac = gt.take(pop_a, axis=1).count_alleles()
+    b_ac = gt.take(pop_b, axis=1).count_alleles()
+    c_ac = gt.take(pop_c, axis=1).count_alleles()
+    # Calculate Fst with corrections.
+    a_b_fst = calc_fst_per_site(a_ac, b_ac)
+    a_c_fst = calc_fst_per_site(a_ac, c_ac)
+    c_b_fst = calc_fst_per_site(c_ac, b_ac)
+    # Calculate PBS.
+    pbs = (
+        ((np.log(1.0 - a_b_fst) * -1.0) +\
+         (np.log(1.0 - a_c_fst) * -1.0) -\
+         (np.log(1.0 - c_b_fst) * -1.0)) / 2.0
+    )
+    # Set the sites that are not segregating to undefined.
+    pbs[~seg_mask] = np.nan
+    return pbs
+
+# Define a function to calculate pbs per snp per chromosome.
+def pbs_chromosome(chrom):
+    # Load in the meta information as a pandas dataframe.
+    meta_df = pd.read_csv(
+        '../meta_data/tgp_mod.txt', sep='\t',
+        names=['IND', 'POP', 'SUPERPOP'],
+    )
+    # Intialize a dictionary to store all sample indicies.
+    samp_idx_dicc = {
+        'MXL_NAT': np.loadtxt('../amr_lai/anc_props/mxl_nat_idx.txt.gz', dtype=int),
+        'MXL_NOT': np.loadtxt('../amr_lai/anc_props/mxl_not_idx.txt.gz', dtype=int),
+    }
+    # For every population...
+    for pop in ['MXL', 'CEU', 'CHB']:
+        # Append the dictionary with sample indicies.
+        samp_idx_dicc[pop] = meta_df[meta_df['POP'] == pop].index.values
+    # Extract the genotype callset and positions.
+    gt, pos = load_gt_pos('tgp_mod_no_aa', chrom)
+    # Intialize a matrix to store the results.
+    results_mat = np.empty((pos.size, 3))
+    # For every ancestry partition.
+    for i, mxl in enumerate(['MXL', 'MXL_NAT', 'MXL_NOT']):
+    # Compute PBS.
+        results_mat[:, i] = calc_pbs_per_site(
+            gt=gt, pop_a=samp_idx_dicc[mxl],
+            pop_b=samp_idx_dicc['CHB'], pop_c=samp_idx_dicc['CEU'],
+        )
+    # Export the results.
+    np.savetxt(
+        f'../muc19_results/tgp_mod_no_aa/mxl_chb_ceu_pbs_chr{chrom}.txt.gz',
+        results_mat, fmt='%1.15f',
+    )
+    return
+
+
+# Calculate pbs.
+pbs_chromosome(chrom=int(sys.argv[1]))
---
+++ pbs/mxl_chb_ceu_pbs_windows_v_revisions.py
@@ -0,0 +1,115 @@
+# Import packages.
+import allel
+import numpy as np
+import numcodecs
+import pandas as pd
+import sys
+import zarr
+
+### sys.argv[1] = chromosome ###
+### sys.argv[2] = window size ###
+
+
+# Define a function to load genotyope and positions arrays.
+def load_callset_pos(prefix, chrom):
+    # Intialize the file path.
+    path = f'../zarr_data/{prefix}_chr{chrom}.zarr'
+    # Load the zarr array.
+    zarr_array = zarr.open_group(path, mode='r')
+    # Extract the genotype callset.
+    callset = zarr_array[f'{chrom}/calldata/GT']
+    # Load the positions.
+    pos = allel.SortedIndex(zarr_array[f'{chrom}/variants/POS'])
+    return callset, pos
+
+# Define a  function to compute Hudson's Fst estimator for a region.
+def calc_fst_per_region(ac1, ac2):
+    # Compute.
+    num, den = allel.hudson_fst(ac1, ac2)
+    # Account for the denominator being zero.
+    fst = np.nansum(num) / np.nansum(den) if np.nansum(den) != 0 else 0
+    # Correct for negative Fst values.
+    return max(fst, 0)
+
+# Define a function to calculate PBS per region.
+def calc_pbs_per_region(gt, pop_a, pop_b, pop_c):
+    # Determine the sample list.
+    samp_list = np.concatenate([pop_a, pop_b, pop_c])
+    # Determine if any of the sites are segregating.
+    seg_mask = gt.take(samp_list, axis=1).count_alleles().is_segregating()
+    # If no sites are segregating.
+    if seg_mask.sum() == 0:
+        # Return undefinded.
+        return np.nan
+    # Else there are segreagting sites.
+    else:
+        # Determine allele counts.
+        a_ac = gt.take(pop_a, axis=1).count_alleles()
+        b_ac = gt.take(pop_b, axis=1).count_alleles()
+        c_ac = gt.take(pop_c, axis=1).count_alleles()
+        # Calculate Fst.
+        a_b_fst = calc_fst_per_region(a_ac, b_ac)
+        a_c_fst = calc_fst_per_region(a_ac, c_ac)
+        c_b_fst = calc_fst_per_region(c_ac, b_ac)
+        # Correct for Fst values of 1 that will lead to inf.
+        a_b_fst = min(a_b_fst, 0.99999)
+        a_c_fst = min(a_c_fst, 0.99999)
+        c_b_fst = min(c_b_fst, 0.99999)
+        # Calculate PBS.
+        pbs = (
+            ((np.log(1.0 - a_b_fst) * -1.0) +\
+             (np.log(1.0 - a_c_fst) * -1.0) -\
+             (np.log(1.0 - c_b_fst) * -1.0)) / 2.0
+        )
+    return pbs
+
+# Define a function to calculate PBS_{MXL:CHB:CEU} in non-overlapping windows.
+def pbs_windows(chrom, window_size):
+    # Load in the meta information as a pandas dataframe.
+    meta_df = pd.read_csv(
+        '../meta_data/tgp_mod.txt', sep='\t',
+        names=['IND', 'POP', 'SUPERPOP'],
+    )
+    # Intialize a dictionary to store all sample indicies.
+    samp_idx_dicc = {
+        'MXL_NAT': np.loadtxt('../amr_lai/anc_props/mxl_nat_idx.txt.gz', dtype=int),
+        'MXL_NOT': np.loadtxt('../amr_lai/anc_props/mxl_not_idx.txt.gz', dtype=int),
+    }
+    # For every population...
+    for pop in ['MXL', 'CEU', 'CHB']:
+        # Append the dictionary with sample indicies.
+        samp_idx_dicc[pop] = meta_df[meta_df['POP'] == pop].index.values
+    # Extract the genotype callset and positions.
+    callset, all_pos = load_callset_pos('tgp_mod_no_aa', chrom)
+    # Load the windows data frame.
+    qc_windows_df = pd.read_csv(f'../windowing/tgp_mod_no_aa/{window_size}kb_nonoverlapping_variant_windows.csv.gz')
+    # Subset the the windows for the chromosome.
+    chr_qc_windows_df = qc_windows_df[qc_windows_df['CHR'] == chrom]
+    # Extract the start and stop positions.
+    chr_starts = chr_qc_windows_df['START'].values
+    chr_stops = chr_qc_windows_df['STOP'].values
+    # Determine the number of winds.
+    n_winds = chr_qc_windows_df.shape[0]
+    # Intialize a matrix to store the results.
+    results_mat = np.empty((n_winds, 3))
+    # For ever window.
+    for wind in range(n_winds):
+        # Locate the window.
+        wind_loc = all_pos.locate_range(chr_starts[wind], chr_stops[wind])
+        # For every ancestry partition.
+        for i, mxl in enumerate(['MXL', 'MXL_NAT', 'MXL_NOT']):
+            # Compute PBS.
+            results_mat[wind, i] = calc_pbs_per_region(
+                gt=allel.GenotypeArray(callset[wind_loc]), pop_a=samp_idx_dicc[mxl],
+                pop_b=samp_idx_dicc['CHB'], pop_c=samp_idx_dicc['CEU'],
+            )
+    # Export the results.
+    np.savetxt(
+        f'../muc19_results/tgp_mod_no_aa/mxl_chb_ceu_pbs_chr{chrom}_{window_size}kb.txt.gz',
+        results_mat, fmt='%1.15f',
+    )
+    return
+
+
+# Calculate pbs.
+pbs_windows(chrom=int(sys.argv[1]), window_size=int(sys.argv[2]))
